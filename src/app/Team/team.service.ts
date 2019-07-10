4920692332d07d83544f3c0f8849ad8dcf3e5f77
Andre Monte <amachado-do-monte@myseneca.ca>
2019-07-10 16:06:57 -0400

    .

--- src/app/Team/team.service.ts
+++ src/app/Team/team.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
-
-  teamUrl: string = "https://delivery.chalk247.com/team_list/NFL.JSON?api_key=74db8efa2a6db279393b433d97c2bc843f8e32b0";
+  teamUrl: string = "https://api.codetabs.com/v1/proxy?quest=https://delivery.chalk247.com/team_list/NFL.JSON?api_key=74db8efa2a6db279393b433d97c2bc843f8e32b0";
 
   constructor(private http: HttpClient) { }
 
   getAllTeams(): Observable<any> {
-    return this.http.get<any>(`${this.teamUrl}`);
+    let cors = "https://api.codetabs.com/v1/proxy?quest="
+/*      let headers = new HttpHeaders().set("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");  */
+    return this.http.get<any>(`${this.teamUrl}`, /* {headers} */);
   }
 
 }
